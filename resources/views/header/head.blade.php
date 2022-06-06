0e771806cd92eca4996e7d55fa5919078d257d92
Naatty123 <nattynengeda@gmail.com>
2022-06-06 13:19:08 +0300

    Random Commit

--- resources/views/header/head.blade.php
+++ resources/views/header/head.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="imgs/main_icon/icon.png">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="css/app.css">
     <script src="{{ mix('/js/app.js') }}"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Zema</title>
