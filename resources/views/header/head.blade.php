d7759c320e92dca1de6324712f9a26dcf534cc93
Naatty123 <nattynengeda@gmail.com>
2022-06-06 13:17:07 +0300

    Fifth

--- resources/views/header/head.blade.php
+++ resources/views/header/head.blade.php
@@ -7,6 +7,7 @@
     <link rel="icon" href="imgs/main_icon/icon.png">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ mix('/js/app.js') }}"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Zema</title>
 </head>
 <body>
