42532c00f32bda7a67c1b80591019202d4baa10a
Naatty123 <nattynengeda@gmail.com>
2022-06-06 13:15:43 +0300

    Fourth commit for the CSS to work

--- resources/views/header/head.blade.php
+++ resources/views/header/head.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="imgs/main_icon/icon.png">
-    <link rel="stylesheet" href="{{ asset('tailcss/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ mix('/js/app.js') }}"></script>
     <title>Zema</title>
 </head>
