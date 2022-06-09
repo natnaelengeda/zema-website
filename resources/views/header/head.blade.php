3849e61ff4f1a61e259fedbfef7005e914c6306c
Naatty123 <nattynengeda@gmail.com>
2022-06-09 22:31:05 +0300

    Icon change

--- resources/views/header/head.blade.php
+++ resources/views/header/head.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="imgs/main_icon/icon.png">
+    <link rel="icon" href="imgs/icon/icon.png">
     <link rel="stylesheet" href="css/app.css">
     <script src="{{ mix('/js/app.js') }}"></script>
     <script src="https://cdn.tailwindcss.com"></script>
